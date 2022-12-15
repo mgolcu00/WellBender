02035fe90cc8333bcc64ff2d29adb92b7e1cc184
Mert Golcu <mert.golcu@locals-MacBook-Pro.local>
2022-12-15 19:39:11 +0300

    add emotion design

--- app/src/main/java/com/mertgolcu/wellbender/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/mertgolcu/wellbender/ui/home/HomeViewModel.kt
@@ -1,6 +1,7 @@
 package com.mertgolcu.wellbender.ui.home
 
 import androidx.lifecycle.MutableLiveData
+import com.mertgolcu.wellbender.R
 import com.mertgolcu.wellbender.core.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -15,11 +16,22 @@ class HomeViewModel @Inject constructor() : BaseViewModel() {
     val userName = MutableLiveData(MOCK_USER_NAME)
     val welcomeMessage = MutableLiveData(MOCK_WELCOME_MESSAGE)
     val avatarUrl = MutableLiveData(MOCK_AVATAR_URL)
-
+    val emojiList = MutableLiveData(MOCK_EMOTION_LIST)
 
     companion object {
         const val MOCK_USER_NAME = "Mert"
         const val MOCK_WELCOME_MESSAGE = "Good Afternoon,"
         const val MOCK_AVATAR_URL = "https://avatars2.githubusercontent.com/u/44591905"
+        val MOCK_EMOTION_LIST = arrayListOf(
+            MockEmotion("Happy", R.drawable.ic_happy_emoji),
+            MockEmotion("Happy", R.drawable.ic_happy_emoji),
+            MockEmotion("Happy", R.drawable.ic_happy_emoji),
+        )
     }
 }
+
+data class MockEmotion(
+    val text: String,
+    val image: Int,
+    val imageUrl: String? = null
+)
