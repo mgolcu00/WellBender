9cf9452f1f0bd87226157f46da614878f5da8924
Mert Golcu <mert.golcu@192.168.1.96>
2022-12-26 11:52:40 +0300

    add splash screen

--- app/src/main/java/com/mertgolcu/wellbender/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/mertgolcu/wellbender/ui/splash/SplashActivity.kt
@@ -4,8 +4,10 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.mertgolcu.wellbender.ui.main.MainActivity
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
 
 /**
  * Created by Mert Gölcü on 19.12.2022.
@@ -16,7 +18,14 @@ import dagger.hilt.android.AndroidEntryPoint
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        startActivity(Intent(this, MainActivity::class.java))
-        finish()
+        lifecycleScope.launchWhenCreated {
+            delay(DELAY_TIME)
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            finish()
+        }
+    }
+
+    companion object {
+        const val DELAY_TIME = 3000L // 3 sec
     }
 }
